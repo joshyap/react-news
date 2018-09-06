ebad41e1ded55f7cddc817df8ab2b1eec577bdd8
Joshua Yap <yosh@MacBook-Pro-3.attlocal.net>
2018-09-05 22:53:45 -0500

    testing jsx variables

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let helloWorld = 'Welcome to the Road to learn React';
+    let person = {
+      name:"Josh",
+      age:"26"
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>{ helloWorld }</h2>
+        <h3>My name is { person.name } and I am { person.age } years old.</h3>
       </div>
     );
   }
