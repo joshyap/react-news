48af2f82576d36a74c89988cbb26f3d3b21828a7
Joshua Yap <yosh@MacBook-Pro-3.attlocal.net>
2018-09-08 23:03:41 -0500

    added hot reloading to indexjs

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ class App extends Component {
       <div className="App">
         <h2>{ helloWorld }</h2>
         <h3>My name is { person.name } and I am { person.age } years old.</h3>
+        <p>this is a test of hot reloading</p>
+        <p><sup>spoiler: it is HOT</sup></p>
       </div>
     );
   }
